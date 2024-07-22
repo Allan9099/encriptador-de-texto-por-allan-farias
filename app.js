4371365fe2dbc7d2f488e51bc0ab5d1545c922c7
Allan Farias <allan.farias02@inacapmail.cl>
2024-07-22 17:02:26 -0400

    punto v0.1 del JS

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let textoUsuario = "";
+let letrasTextoUsuario = [];
+let textoNuevo = "";
+
 function funcionHTML (elemento,texto) {
 
     let elementoHTML = document.querySelector(elemento);
@@ -13,5 +17,25 @@ function condicionesIniciales() {
     return;
 
 }
+
+function logicaEncriptado() {
+
+    textoUsuario = document.getElementById("texto").value;
+
+    letrasTextoUsuario = textoUsuario.split("");
+
+    /*console.log(`este es el texto original: "${textoUsuario}"`)
+    console.log(`este es el texto por sus letras: "${letrasTextoUsuario}".`);*/
+    textoNuevo = letrasTextoUsuario.join("");
+    /*console.log(`y este el texto ya rearmado: "${textoNuevo}"`);*/
+
+    return;
+}
+
+/*function test(){
+
+    alert("hola mundo!");
+
+}*/
 
 condicionesIniciales();
